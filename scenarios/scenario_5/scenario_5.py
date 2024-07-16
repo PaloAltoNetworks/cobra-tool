2da67ef2494e088a561dcc49e0c1207c4320be24
Harsha Koushik <harshakoushik7499@gmail.com>
2024-07-16 18:52:50 +0530

    Update infra

--- scenarios/scenario_5/scenario_5.py
+++ scenarios/scenario_5/scenario_5.py
@@ -11,24 +11,58 @@ from core.helpers import loading_animation
 from core.helpers import generate_ssh_key
 
 def scenario_5_execute():
-    print("-"*30)
-    print(colored("Executing Scenraio 5 : Compromise instance, takover, use s3 access, perform ransomware with external kms key", color="red"))
-    loading_animation()
-    print("-"*30)
+    # print("-"*30)
+    # print(colored("Executing Scenraio 5 : Compromise instance, takover, use s3 access, perform ransomware with external kms key", color="red"))
+    # loading_animation()
+    # print("-"*30)
 
-    print(colored("Rolling out Infra", color="red"))
-    loading_animation()
-    print("-"*30)
+    # print(colored("Rolling out Infra", color="red"))
+    # loading_animation()
+    # print("-"*30)
     
-    file_path = "./core/aws-scenario-5-output.json"
-    if os.path.exists(file_path):
-        os.remove(file_path)
-        print("File '{}' found and deleted.".format(file_path))
-    else:
-        print("File '{}' not found.".format(file_path))
+    # file_path = "./core/aws-scenario-5-output.json"
+    # if os.path.exists(file_path):
+    #     os.remove(file_path)
+    #     print("File '{}' found and deleted.".format(file_path))
+    # else:
+    #     print("File '{}' not found.".format(file_path))
 
-    generate_ssh_key()
+    # generate_ssh_key()
 
-    subprocess.call("cd ./scenarios/scenario_5/infra/ && pulumi up -s aws-scenario-5 -y", shell=True)
-    subprocess.call("cd ./scenarios/scenario_5/infra/ && pulumi stack -s aws-scenario-5 output --json >> ../../../core/aws-scenario-5-output.json", shell=True)
+    # subprocess.call("cd ./scenarios/scenario_5/infra/ && pulumi up -s aws-scenario-5 -y", shell=True)
+    # subprocess.call("cd ./scenarios/scenario_5/infra/ && pulumi stack -s aws-scenario-5 output --json >> ../../../core/aws-scenario-5-output.json", shell=True)
+
+    with open("./core/aws-scenario-5-output.json", "r") as file:
+        data = json.load(file)
+
+    ATTACKER_SERVER_PUBLIC_IP = data["Attacker Server Public IP"] 
+    BUCKET_NAME = data["Bucket Name"]   
+    KMS_KEY="arn:aws:kms:us-east-1:829815287709:key/mrk-e924ca7d0bd24ea3af12e0bb47602321"
+
+    # sleep_duration = 80
+    # with tqdm(total=sleep_duration, desc="Infra coming up") as pbar:
+    #     while sleep_duration > 0:
+    #         sleep_interval = min(1, sleep_duration)
+    #         sleep(sleep_interval)
+            
+    #         pbar.update(sleep_interval)
+    #         sleep_duration -= sleep_interval    
+
+    #Add Webapp attack here later
+    print(colored("Initiate Instance Takeover", color="red"))
+    loading_animation()
+    print("-"*30)
+    #subprocess.call(f"ssh -o 'StrictHostKeyChecking accept-new' -i ./id_rsa ubuntu@{ATTACKER_SERVER_PUBLIC_IP} ''", shell=True)
+
+
+    print(colored("Access bucket & encrypt the objects using external kms key", color="red"))
+    loading_animation()
+    print("-"*30)
+    subprocess.call(f"ssh -o 'StrictHostKeyChecking accept-new' -i ./id_rsa ubuntu@{ATTACKER_SERVER_PUBLIC_IP} 'echo BUCKET_NAME={BUCKET_NAME}>>~/.bashrc && echo KMS_KEY={KMS_KEY}>>~/.bashrc'", shell=True)
+    subprocess.call(f"ssh -o 'StrictHostKeyChecking accept-new' -i ./id_rsa ubuntu@{ATTACKER_SERVER_PUBLIC_IP} 'source ~/.bashrc && echo $BUCKET_NAME'", shell=True)
+
+
+
+
+
 
