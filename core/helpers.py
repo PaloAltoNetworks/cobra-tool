0715dad717d1b1a22e3be2b42a8fe327065ddef2
Matt Dorn <matt.dorn@gmail.com>
2024-07-29 07:06:54 -0500

    Clean up unneeded code, various other cleanup

--- core/helpers.py
+++ core/helpers.py
@@ -1,22 +1,15 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from pathlib import Path, PurePath
 import pyfiglet
 import re
 import subprocess
-import re
-from time import sleep
 import time
 
 from pathlib import Path, PurePath
 from termcolor import colored
 from time import sleep
 
-import pyfiglet
-import requests
-import yaml
-from tqdm import tqdm
 from tqdm import tqdm
 import requests
 import yaml
@@ -62,54 +55,6 @@ def generate_ssh_key():
         subprocess.run(["ssh-keygen", "-t", "rsa", "-b", "4096", "-N", "", "-f", key_path], stdout=devnull, stderr=devnull)
     print("SSH Key Pair generated successfully!")
     return key_path, key_path + ".pub"
-
-
-def slugify(s):
-    s = s.lower().strip()
-    s = re.sub(r'[^\w\s-]', '', s)
-    s = re.sub(r'[\s_-]+', '-', s)
-    s = re.sub(r'^-+|-+$', '', s)
-    return s
-
-
-def http_request(url, method='GET', headers={}):
-    resp = requests.request(method, url)
-    return resp
-
-
-def get_scenario_list():
-    """Get a list of scenarios for selection from command line.
-    """
-    scenarios_path = Path(__file__).parent.parent / 'scenarios_ng'
-    scenarios_list = []
-    for path in Path(scenarios_path).glob('*'):
-        if os.path.isdir(path):
-            scenarios_list.append(PurePath(path).name)
-    scenarios_list.sort()
-    return scenarios_list
-
-
-def get_scenarios_config():
-    """Create dict containing scenario config data from every entry in
-    scenarios directory (title, description, etc.)
-    """
-    scenarios_path = Path(__file__).parent.parent / 'scenarios_ng'
-    scenarios_config = {}
-    for path in Path(scenarios_path).glob('*'):
-        if os.path.isdir(path):
-            scenario_name = PurePath(path).name
-            scenario_path = Path(scenarios_path) / scenario_name
-            config_path = Path(scenario_path, '_files', 'config.yaml')
-            with open(config_path, 'r') as file_:
-                config = yaml.load(file_, Loader=yaml.SafeLoader)
-            scenarios_config[scenario_name] = config
-    return scenarios_config
-
-
-def print_ascii_art(text):
-    ascii_art = pyfiglet.figlet_format(text)
-    print(colored(ascii_art, color="cyan"))
-
 
 
 def slugify(s):
