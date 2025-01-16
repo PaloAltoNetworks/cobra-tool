aa650d4c805e06757eac163f13c707fa31aa0030
Harsha Koushik <68949130+hkoushik@users.noreply.github.com>
2025-01-16 17:16:03 +0530

    Update __main__.py

--- scenarios/scenario_7/infra/__main__.py
+++ scenarios/scenario_7/infra/__main__.py
@@ -1,19 +1,40 @@
+import iam
+import vpc
+import utils
 import pulumi
-import pulumi_eks as eks
-import pulumi_aws as aws
- 
+from pulumi_aws import eks
+
 # Create an EKS cluster with the default configuration.
 current = aws.get_region()
 region = current.name
 
+eks_cluster = eks.Cluster(
+    "eks-cluster",
+    role_arn=iam.eks_role.arn,
+    tags={
+        "Name": "pulumi-eks-cluster",
+    },
+    vpc_config=eks.ClusterVpcConfigArgs(
+        public_access_cidrs=["0.0.0.0/0"],
+        security_group_ids=[vpc.eks_security_group.id],
+        subnet_ids=vpc.subnet_ids,
+    ),
+)
 
-cluster = eks.Cluster("cluster", 
-                      tags={
-        'Name': 'backend-cluster',
-    },)
-#print(cluster.resource_name)
-
-# Export the cluster's kubeconfig.
-#pulumi.export("kubeconfig", cluster.kubeconfig)
+eks_node_group = eks.NodeGroup(
+    "eks-node-group",
+    cluster_name=eks_cluster.name,
+    node_group_name="pulumi-eks-nodegroup",
+    node_role_arn=iam.ec2_role.arn,
+    subnet_ids=vpc.subnet_ids,
+    tags={
+        "Name": "pulumi-cluster-nodeGroup",
+    },
+    scaling_config=eks.NodeGroupScalingConfigArgs(
+        desired_size=2,
+        max_size=2,
+        min_size=1,
+    ),
+)
 pulumi.export("Region", region)
-pulumi.export("ClusterData", cluster.core)
+pulumi.export("ClusterData", utils.generate_kube_config(eks_cluster))
